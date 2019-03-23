e410d35c9efe0569c458db9be08afa0cce0ac06d
danny <danixeka@gmail.com>
2019-03-23 11:47:57 +1100

    preparing views

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
+const path = require('path'); // core module
 const express = require('express');
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
   res.send(
-    '<h1>Hello World</h1>'
+    '<h1>Weather</h1>'
   )
-})
+});
 
 app.get('/help', (req, res) => {
   res.send([{
@@ -16,19 +20,18 @@ app.get('/help', (req, res) => {
     name: 'Fellini',
     age: 44,
   }])
-})
+});
 
 app.get('/about', (req, res) => {
   res.send('<h1>About Page</h1>')
-})
+});
 
 app.get('/weather', (req, res) => {
   res.send([{
     forecast: 'I have no idea',
     location: 'Sydney',
   }])
-})
-
+});
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
