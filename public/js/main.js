e9510bbbc48fd571ba53bafef7a4d8bd0c2413f7
danny <danixeka@gmail.com>
2019-03-24 16:51:51 +1100

    created UI for the forecast

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,29 @@
-console.log('allora!');
-
-
-
 window.onload = function () {
 
   const weatherForm = document.querySelector('form');
   const search = document.querySelector('input');
+  const msgOne = document.getElementById('msg-one');
+  const msgTwo = document.getElementById('msg-two');
 
   weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const location = search.value;
 
+    msgOne.textContent = 'Loading...';
+    msgTwo.textContent = '';
+
     fetch('http://localhost:3000/weather?address=' + location)
     .then( (response) => {
       response.json()
       .then( (data) => {
         if (data.error) {
-          console.log(data.error);
+          msgOne.textContent = data.error;
         } else {
-          console.log(data.location);
-          console.log(data.forecast);
+          msgOne.textContent = data.location;
+          msgTwo.textContent = data.forecast;
         }
-
       })
     })
-
   });
-
-
 }
