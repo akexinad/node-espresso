8f4e57decc52c623159630530ee2b5210cc8bb20
danny <danixeka@gmail.com>
2019-03-24 14:56:37 +1100

    HTTP requests with fetch

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+console.log('allora!');
+
+fetch('http://puzzle.mead.io/puzzle')
+.then( (response) => {
+  response.json()
+  .then( (data) => {
+    console.log(data)
+  })
+})
+
+fetch('http://localhost:3000/weather?address=siena')
+.then( (response) => {
+  response.json()
+  .then( (data) => {
+    if (data.error) {
+      return console.log(data.error);
+    }
+
+    console.log(data.location);
+    console.log(data.forecast);
+  })
+})
