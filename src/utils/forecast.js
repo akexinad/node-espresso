297164cda6ba47e7a9c3a24869d211526da37c14
danny <danixeka@gmail.com>
2019-03-25 14:34:40 +1100

    added more data to weather summary

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,8 @@
 const request = require('request');
 const chalk = require('chalk');
 
+// TEST API => https://api.darksky.net/forecast/8f309f032cb56d3ea822766f089e0e51/33.8688,151.2093
+
 const forecast = (latitude, longitude, callback) => {
   const url = `https://api.darksky.net/forecast/8f309f032cb56d3ea822766f089e0e51/${ latitude },${ longitude }?units=si`;
 
@@ -15,7 +17,7 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       const [ current, daily ] = [ body.currently, body.daily.data[0] ];
 
-      callback(undefined, daily.summary + ' Current temperature is ' + current.temperature + ' degrees celcius. There is a ' + current.precipProbability + ' percent chance of rain.')
+      callback(undefined, daily.summary + ' Current temperature is ' + current.temperature + ' degrees celcius. There is a ' + current.precipProbability + ' percent chance of rain.' + ' High is predicted to be ' + daily.temperatureHigh + ' with a low of ' + daily.temperatureLow + ' degrees.')
     }
   })
 }
